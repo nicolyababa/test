d635c9e930a188aea0c42a763c02f0fca1e1f0ef
nicolyababa <38101737+nicolyababa@users.noreply.github.com>
2020-11-26 15:38:42 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ссылку: ");
+            string url = Console.ReadLine();       // вводим имя
+        }
+
+        // <summary>
+        /// Отправка GET-запроса на сторонние сервисы.
+        /// </summary>
+        /// <param name="url">Url адрес с параметрами куда отправляем запрос</param>
+        /// <returns>Код страницы в строковом формате.</returns>
+        public string SendGetRequest(string url)
+        {
+            var webRequest = (System.Net.HttpWebRequest)WebRequest.Create(url);
+
+            var result = string.Empty;
+            using (var response = (HttpWebResponse)webRequest.GetResponse())
+            {
+                using (var stream = response.GetResponseStream())
+                {
+                    if (stream != null)
+                    {
+                        using (var streamReader = new StreamReader(stream))
+                        {
+                            result = streamReader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return result;
+       
